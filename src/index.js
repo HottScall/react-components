260921626ef8f05a5acabbee8dc419e6c84da24f
Scott <Scotthall1983@hotmail.com>
2019-10-14 08:54:42 +0100

    Wrapped the remainder of the CommentDetail jsx calls with ApprovaCard jsx calls to ensure all comments are wrapped in the new ApprovalCard component.

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ const App = () => {
       <ApprovalCard>
         <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva" avatar={faker.image.avatar()} />
       </ApprovalCard>
-        <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo" avatar={faker.image.avatar()} />
-        <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props" avatar={faker.image.avatar()} />
 
+      <ApprovalCard>
+        <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props" avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 };
