f2b0c129c9c8e337541b253a3efe1854dda61ccc
Scott <Scotthall1983@hotmail.com>
2019-10-14 08:51:46 +0100

    Wrapped a single CommentDetail component within an ApprovalCard component within Index.js. Added a props argument to the ApprovalCard.js file, used console.log(props) in ApprovalCard.js to ensure that the CommentDetail component is rendering to the console. Then removed the hardcoded 'Are you sure?' and replaced it with a jsx call called {props.children}

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="UI container comments">
-      <ApprovalCard />
-      <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva" avatar={faker.image.avatar()} />
-      <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo" avatar={faker.image.avatar()} />
-      <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props" avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+        <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo" avatar={faker.image.avatar()} />
+        <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props" avatar={faker.image.avatar()} />
+
     </div>
   );
 };
