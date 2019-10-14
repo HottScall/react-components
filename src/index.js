659c260dff2e12aaa0af620453acfa0df59619b1
Scott <Scotthall1983@hotmail.com>
2019-10-14 08:32:58 +0100

    Added a new component called ApprovalCard which wraps around the CommentDetail component. This will allow for comments to be approved or rejected. Used Semantic UI to find the class 'ui basic green button' which renders content from Semantic UI to make approval buttons. Hardcoded text to buttons to ensure it's rendering

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="UI container comments">
+      <ApprovalCard />
       <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva" avatar={faker.image.avatar()} />
       <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo" avatar={faker.image.avatar()} />
       <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props" avatar={faker.image.avatar()} />
