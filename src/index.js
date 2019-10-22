087f9a9676aa9d1088c814effd0381ef0dfe3639
Scott <Scotthall1983@hotmail.com>
2019-10-22 12:02:21 +0100

    Added an export statement at the bottom of comment detail and then imported the CommentDetail file into the index.js file. Replaced the lengthy code for each comment with a simple component call, <CommentDetail />

--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
 const App = () => {
   return (
     <div className="UI container comments">
-      <div className="comment">
-        <a href="/" className="Avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00 PM</span>
-          </div>
-            <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <a href="/" className="Avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00 PM</span>
-          </div>
-            <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
 
   );
