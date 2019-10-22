4859626de0620330194c8e527dd0138f870cf3d2
Scott <Scotthall1983@hotmail.com>
2019-10-22 11:55:01 +0100

    Added a new component file called CommentDetail which will take all the code for the comment. Imported the faker account to the comment detail file.

--- src/index.js
+++ src/index.js
@@ -19,6 +19,21 @@ const App = () => {
             <div className="text">Nice blog post!</div>
         </div>
       </div>
+
+      <div className="comment">
+        <a href="/" className="Avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+            <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     </div>
 
   );
