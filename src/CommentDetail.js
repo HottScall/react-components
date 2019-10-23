dd0a8e478205658535f9dbe0034d9eca31bd5633
Scott <Scotthall1983@hotmail.com>
2019-10-23 21:59:21 +0100

    Added a new Avatar attribute to the comment detail jsx call in index.js. Called props.avatar in commentdetail.js

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -2,23 +2,22 @@ import React from 'react';
 import faker from 'faker';
 
 const CommentDetail = (props) =>{
-  console.log(props);
   return (
     <div className="comment">
-      <a href="/" className="Avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
-        <div className="metadata">
-          <span className="date">{props.timeAgo}</span>
-        </div>
-          <div className="text">{props.authComm}</div>
+          <div className="metadata">
+            <span className="date">{props.timeAgo}</span>
+          </div>
+            <div className="text">{props.authComm}</div>
       </div>
     </div>
-  )
+  );
 };
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 import faker from 'faker';
-import CommentDetail from './CommentDetail'
 
 const App = () => {
   return (
     <div className="UI container comments">
-      <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva"/>
-      <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo"/>
-      <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props"/>
+      <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva" avatar={faker.image.avatar()} />
+      <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo" avatar={faker.image.avatar()} />
+      <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props" avatar={faker.image.avatar()} />
     </div>
-
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
