956a6f99a069af31c1667d7562168fda44b4f56c
Scott <Scotthall1983@hotmail.com>
2019-10-23 20:29:22 +0100

    Added a timeAgo attribute to the Comment Detail in Index.js and added a fake date and time for each post. In comment detail I removed the hardcoded time and replaced with props.timeAgo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail'
 const App = () => {
   return (
     <div className="UI container comments">
-      <CommentDetail author="Scott" />
-      <CommentDetail author="Frank" />
-      <CommentDetail author="Catty" />
+      <CommentDetail author="Scott" timeAgo="Today at 14:45" />
+      <CommentDetail author="Frank" timeAgo="Yesterday at 19:55"/>
+      <CommentDetail author="Catty" timeAgo="Wednesday at 08:23"/>
     </div>
 
   );
