2f99e9a53dd57f2e4cf3454126cf43995f770de9
Scott <Scotthall1983@hotmail.com>
2019-10-23 20:34:46 +0100

    Added a new attribute to CommentDetail component call in Index.js. Added a different comment in each one. Used props.authComm in CommentDetail.js to call those comments from the parent component.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail'
 const App = () => {
   return (
     <div className="UI container comments">
-      <CommentDetail author="Scott" timeAgo="Today at 14:45" />
-      <CommentDetail author="Frank" timeAgo="Yesterday at 19:55"/>
-      <CommentDetail author="Catty" timeAgo="Wednesday at 08:23"/>
+      <CommentDetail author="Scott" timeAgo="Today at 14:45" authComm="Nice one bruva"/>
+      <CommentDetail author="Frank" timeAgo="Yesterday at 19:55" authComm="Sweet photo"/>
+      <CommentDetail author="Catty" timeAgo="Wednesday at 08:23" authComm="This comment uses props"/>
     </div>
 
   );
